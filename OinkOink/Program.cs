99bd357055b177e488587479e264534fbc99cac3
YasaSun <148882912+YasaSun@users.noreply.github.com>
2026-02-17 14:36:30 +0500

    Update Program.cs

--- OinkOink/Program.cs
+++ OinkOink/Program.cs
@@ -3,37 +3,25 @@ using System;
 
 class User
 {
-    
     public string Login { get; }
-
-    
     protected int AccessLevel;
-
     
     public User(string login, int accessLevel)
     {
-        
         Login = login;
-
-        
         AccessLevel = accessLevel;
     }
-
-    
     public virtual void ShowInfo()
     {
         Console.WriteLine(
             $"Пользователь: {Login}, уровень доступа: {AccessLevel}"
         );
     }
-
-    
     public virtual bool CanEdit()
     {
         return false;
     }
 }
-
 
 class Admin : User
 {
@@ -42,22 +30,17 @@ class Admin : User
         : base(login, 10)
     {
     }
-
     
     public override bool CanEdit()
     {
-        
         return true;
     }
-
-    
     public override void ShowInfo()
     {
         
         Console.WriteLine($"Администратор: {Login}, полный доступ");
     }
 }
-
 class Guest : User
 {
     public Guest(string login)
@@ -71,22 +54,14 @@ class Program
 {
     static void Main()
     {
-      
         User[] users =
         {
-            
             new Admin("boss"),
-
-            
             new Guest("petuh")
         };
-
         foreach (User user in users)
         {
-           
             user.ShowInfo();
-
-          
             bool canEdit = user.CanEdit();
 
             Console.WriteLine($"Может редактировать: {canEdit}");
